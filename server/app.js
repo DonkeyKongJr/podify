f69873ef08a9eb797aede46316e1b795ee99fd05
Patrick Schadler <patrick.schadler@gmail.com>
2019-11-19 22:18:28 +0100

    add express server. change directory structure

---
+++ server/app.js
@@ -0,0 +1,22 @@
+var express = require("express");
+var app = express();
+
+app.use(express.json());
+
+const port = 3000;
+
+app.get("/", function(req, res) {
+  res.send("This server provides an interface to a Text-To-Speech (TTS) API.");
+});
+
+// TODO: Add API Calls to Text-To-Speech API.
+app.post("/tts", function(req, res) {
+  res.send({
+    message: "This method is not implemented yet",
+    requestBody: req.body
+  });
+});
+
+app.listen(3000, function() {
+  console.log(`Text To Speech API listening on ${port}`);
+});
